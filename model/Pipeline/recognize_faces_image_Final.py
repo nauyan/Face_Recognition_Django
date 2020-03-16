5fb6e00ec99d89c1f5d18fde775b792f21a0a6e7
Nauyan <nauyan@hotmail.com>
2020-03-16 16:01:04 +0500

    13-March-2020

--- model/Pipeline/recognize_faces_image_Final.py
+++ model/Pipeline/recognize_faces_image_Final.py
@@ -6,6 +6,7 @@ import face_recognition
 import argparse
 import pickle
 import cv2
+import os
 
 class my_dictionary(dict):  
   
@@ -19,6 +20,7 @@ class my_dictionary(dict):
 
 def inference():
 	# construct the argument parser and parse the arguments
+	"""
 	ap = argparse.ArgumentParser()
 	ap.add_argument("-e", "--encodings", required=True,
 	help="path to serialized db of facial encodings")
@@ -27,21 +29,26 @@ def inference():
 	ap.add_argument("-d", "--detection-method", type=str, default="cnn",
 	help="face detection model to use: either `hog` or `cnn`")
 	args = vars(ap.parse_args())
+	"""
 
 	# load the known faces and embeddings
 	print("[INFO] loading encodings...")
-	data = pickle.loads(open(args["encodings"], "rb").read())
+	#data = pickle.loads(open(args["encodings"], "rb").read())
+	data = pickle.loads(open(os.path.abspath(os.path.dirname(__file__))+"/encodings.pickle", "rb").read())
 
 	# load the input image and convert it from BGR to RGB
-	image = cv2.imread(args["image"])
+	#image = cv2.imread(args["image"])examples/example_01.png
+	image = cv2.imread(os.path.abspath(os.path.dirname(__file__))+"/examples/example_01.png")
 	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 	# detect the (x, y)-coordinates of the bounding boxes corresponding
 	# to each face in the input image, then compute the facial embeddings
 	# for each face
 	print("[INFO] recognizing faces...")
+	#boxes = face_recognition.face_locations(rgb,
+	#	model=args["detection_method"])
 	boxes = face_recognition.face_locations(rgb,
-		model=args["detection_method"])
+		model="cnn")
 	encodings = face_recognition.face_encodings(rgb, boxes)
 
 	# initialize the list of names for each face detected
@@ -95,4 +102,4 @@ def inference():
 	#cv2.waitKey(0)
 	return dictionary_person
 
-print(inference())
+#print(inference())
--- model/url.py
+++ model/url.py
@@ -3,4 +3,5 @@ from django.conf.urls import include, url
 urlpatterns = [
     url(r'^hello/', 'model.views.hello', name = 'hello'),
     url(r'^detect/', 'model.views.detect', name = 'detect'),
+    url(r'^detect_api/', 'model.views.detect_api', name = 'detect_api'),
 ]
